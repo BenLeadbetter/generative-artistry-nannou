14aa4e27d74554bba686fee5c41e4efb638955ad
Ben Leadbetter <ben_leadbetter@hotmail.com>
2023-05-28 15:02:47 +0200

    improve joy-devision implementation
    * generate splines with model rather than on draw
    * use nannour point types in splines rather than cgmath types

--- src/joy_division.rs
+++ src/joy_division.rs
@@ -1,7 +1,7 @@
 use nannou::prelude::*;
 use rand::Rng;
 
-type Line = Vec<cgmath::Vector2<f32>>;
+type Line = Vec<Point2>;
 
 pub fn run() {
     nannou::app(generate_lines)
@@ -11,53 +11,48 @@ pub fn run() {
 }
 
 fn draw_line(line: &Line, draw: &Draw) {
-    // todo: spline interpolation should be down on model creation
-    // not during the draw call
-    let spline = {
-        use splines::*;
-        let step = 1.0_f32 / line.len() as f32;
-        Spline::from_vec(
-            line.iter()
-                .enumerate()
-                .map(|(i, v)| Key::new(i as f32 * step, *v, Interpolation::CatmullRom))
-                .collect(),
-        )
-    };
-    let points = {
-        let resolution = 150_usize;
-        let step = 1.0_f32 / resolution as f32;
-        let mut ret = Vec::new();
-        for i in 0..=resolution {
-            if let Some(pt) = spline.sample(i as f32 * step) {
-                ret.push(pt2(pt.x, pt.y));
-            }
-        }
-        ret
-    };
-    draw.polyline().weight(2_f32).points(points);
+    draw.polyline().weight(2_f32).points(line.clone());
 }
 
 fn generate_lines(app: &App) -> Vec<Line> {
     let n = 30;
-    let line_resolution = 20;
+    let m = 20;
+    let line_resolution = 200;
+    let step = 1.0_f32 / m as f32;
     let height = app.main_window().inner_size_points().0;
     let vstep = height * 0.85 / (n as f32);
-    let hstep = height / (line_resolution as f32);
+    let hstep = height / (m as f32);
     let vstart = -height / 2_f32 + 50.0;
     let hstart = -height / 2_f32;
     let mut rng = rand::thread_rng();
+
+    let mut make_spline = |index: i32| {
+        use splines::*;
+        let mut make_point = |j: i32| {
+            let variance = (m / 2 - (j - m / 2).abs()) as f32;
+            let deviation =
+                rng.gen_range(0.0_f32..1.0_f32) * vstep * variance * variance * 0.05_f32;
+            pt2(
+                hstart + (j as f32) * hstep,
+                vstart + vstep * (index as f32) + deviation,
+            )
+        };
+        Spline::from_vec(
+            (0..=m)
+                .map(|j| Key::new(j as f32 * step, make_point(j), Interpolation::CatmullRom))
+                .collect(),
+        )
+    };
+
     let mut ret = Vec::new();
     for i in 0..n {
-        let mut line = Vec::new();
-        for j in 0..=line_resolution {
-            let variance = (line_resolution / 2 - (j - line_resolution / 2).abs()) as f32;
-            let deviation = rng.gen_range(0.0_f32..1.0_f32) * vstep * variance * variance * 0.05_f32;
-            line.push(cgmath::Vector2 {
-                x: hstart + (j as f32) * hstep,
-                y: vstart + vstep * (i as f32) + deviation,
-            })
-        }
-        ret.push(line);
+        let spline = make_spline(i);
+        ret.push(
+            (0..line_resolution)
+                .map(|index| spline.sample(index as f32 / line_resolution as f32))
+                .filter_map(|x| x)
+                .collect(),
+        );
     }
     ret
 }
